eb37db8af9629aca8816afe5c6c3c15269cff7d3
Jagitr <jg.tumanda@gmail.com>
2025-11-07 14:25:33 +0800

    application py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-test
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import Qt
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()        
+        loadUi("userinterface.ui", self)    
+
+
+app = QApplication(sys.argv)
+ui = MainWindow()
+ui.show()
+sys.exit(app.exec_())
